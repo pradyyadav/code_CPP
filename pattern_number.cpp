891d2fc28c86f082336ee38b6de061e8fd77e0a0
Prad250100 <pradyumna.aky@gmail.com>
2020-01-13 19:26:38 +0530

    new

---
+++ pattern_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a=1;
+    cout<<"Enter number of line : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<a++<<" ";
+        }
+        cout<<endl;
+    }
+}

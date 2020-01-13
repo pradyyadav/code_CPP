5ac31d901c6a7d9868d3f5a709b343e369cd39d6
Prad250100 <pradyumna.aky@gmail.com>
2020-01-13 20:07:50 +0530

    changed void to int

--- starpyramid.cpp
+++ starpyramid.cpp
@@ -1,2 +1,16 @@
 #include<iostream>
 using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of lines : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}

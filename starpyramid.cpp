8ba61367b7d51cd915259da2c9439573e32c57b9
Prad250100 <pradyumna.aky@gmail.com>
2020-01-13 20:28:00 +0530

    new

--- starpyramid.cpp
+++ starpyramid.cpp
@@ -5,12 +5,15 @@ int main()
     int n;
     cout<<"Enter number of lines : ";
     cin>>n;
-    for(int i=1;i<=n;i++)
+    for(int i=1,k=0;i<=n;i++,k=0)
     {
-        for(int j=1;j<=n-i;j++)
+        for(int j=1;j<=(n-i);j++)
         {
             cout<<" ";
         }
+        while(k!=(2*i)-1)
+            {cout<<"*";
+            k++;}
         cout<<endl;
     }
 }

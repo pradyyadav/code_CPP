ff295280c773538232dcd04b82d305436edafbc6
Prad250100 <pradyumna.aky@gmail.com>
2020-03-14 11:44:35 +0530

    function created

--- cipher.cpp
+++ cipher.cpp
@@ -1,26 +1,38 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
+#include<string>
+
+
 using namespace std;
+int CEASER_CIPHER(string text,int s);
+
 int main()
 {
 	int ascii,shift;
-	string text;
+	string user_text;
 	cout<<"Enter your text : ";
-	cin>>text;
+	cin>>user_text;
 	cout<<"\nEnter shifting pattern : ";
 	cin>>shift;
-	for(int i=0;i<text.length();i++)
-	{
-		ascii=text[i];
-		if(isupper(text[i]))
-		{
-			text[i]=((ascii + shift - 65)%26) + 65;
-		}		
-		else
-		{
-			text[i]=((ascii + shift - 97)%26) + 97;
-		}
-	}
-	cout<<text<<"\n";
+	CEASER_CIPHER(user_text,shift);
+	
+}
+int CEASER_CIPHER(string text,int s)
+{
+    int ascii;
+    for(int i=0;i<text.length();i++)
+    {
+        ascii=text[i];
+        if(isupper(text[i]))
+        {
+            text[i]=((ascii + s - 65)%26) + 65;
+        }
+        else
+        {
+            text[i]=((ascii + s - 97)%26) + 97;
+        }
+    }
+    cout<<text<<"\n";
+    return 0;
 }
